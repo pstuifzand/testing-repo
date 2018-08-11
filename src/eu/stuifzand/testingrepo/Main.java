83fc1521ee17fc1ded4dded18d9d230bc623f5b5
Peter Stuifzand <peter@stuifzand.eu>
2018-08-11 21:27:24 +0200

    Show time in message

--- src/eu/stuifzand/testingrepo/Main.java
+++ src/eu/stuifzand/testingrepo/Main.java
@@ -1,7 +1,12 @@
 package eu.stuifzand.testingrepo;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Date date = new Date();
+        DateFormat df = DateFormat.getDateTimeInstance();
+        System.out.printf("[%s] Hello, world!", df.format(date));
     }
 }
