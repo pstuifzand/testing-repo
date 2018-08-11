44cb304a7a290d6726d9200b339d270326de7664
Peter Stuifzand <peter@stuifzand.eu>
2018-08-11 21:23:10 +0200

    improve message

--- src/eu/stuifzand/testingrepo/Main.java
+++ src/eu/stuifzand/testingrepo/Main.java
@@ -2,7 +2,6 @@ package eu.stuifzand.testingrepo;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("HELLO WORLD");
+        System.out.println("Hello, world!");
     }
 }
